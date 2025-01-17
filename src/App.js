63cb1b4aa9cfd2083887a141bbf884818b9e0d2f
Anonymousbeliever <[jkcharo18@gmail.com protected]>
2025-01-17 12:38:46 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+          </Switch>
+
+        </div>
+      </div>
+    </Router>
+
   );
 }
 
